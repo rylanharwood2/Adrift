3d301ce38503c3bd24f288730302108115245adc
rylanharwood2 <ruthlessrylan20@gmail.com>
2024-11-03 15:55:33 -0800

    adding new ship sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,15 +2,16 @@ extends CharacterBody2D
 
 const SPEED = 8000.0
 var bullet_type = ""
+var holding_direction = ""
 
 var scene = preload("res://scenes/bullet.tscn")
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 func shoot(_bullet_typey):
-	#var scene = load("res://scenes/bullet.tscn")
 	var bullet = scene.instantiate()
-	#bullet.position = $Marker2D.global_position()
 	owner.add_child(bullet)
-	bullet.transform = $Marker2D.global_transform
+	bullet.transform = $Muzzle.global_transform
 	print("created bullet")
 	
 
@@ -19,9 +20,20 @@ func _physics_process(delta: float) -> void:
 	
 	if direction: 
 		velocity = direction * SPEED * delta
+		if direction[0] == -1 and holding_direction != "left":
+			animated_sprite.play("tilt_left")
+			holding_direction = "left"
+		if direction[0] == 1 and holding_direction != "right":
+			animated_sprite.play("tilt_right")
+			holding_direction = "right"
+		if direction[1] == 1:
+			pass
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		holding_direction = ""
+		animated_sprite.play("idle")
+		
 		
 	if Input.is_action_just_pressed("shoot"):
 		shoot("")
