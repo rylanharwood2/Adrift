0d43c26b27dd8c4adbd1b5944e9a5963a95dd470
rylanharwood2 <ruthlessrylan20@gmail.com>
2025-11-03 09:34:20 -0800

    proxy mine new explosion and arming animation

--- scripts/proxy_mine.gd
+++ scripts/proxy_mine.gd
@@ -6,7 +6,9 @@ var current_state : MineState = MineState.SPAWNING
 @export var arming_delay : float = 2
 @export var explosion_damage : int = 2
 @export var explosion_radius : int = 125
+var draw_size : float = 0
 @export var lifetime_after_armed : float = 25.
+var should_rotate : bool = false
 
 var who_boom : Node2D
 
@@ -16,11 +18,22 @@ func _ready():
 	change_state(MineState.ARMING)
 
 func _process(delta: float) -> void:
-	rotation_degrees += 15 * delta
+	rotate_mine(delta)
 	queue_redraw()
-	
+
+# draw the red line animation on mine ARMED
 func _draw():
-	draw_circle(Vector2.ZERO, explosion_radius, Color.RED, false)
+	if current_state == MineState.ARMED:
+		if draw_size +1 <= explosion_radius:
+			if $drawing_test.is_stopped():
+				$drawing_test.start(0.01)
+			
+			draw_circle(Vector2.ZERO, draw_size, Color.RED, false)
+		draw_circle(Vector2.ZERO, draw_size, Color.RED, false)
+
+func _on_drawing_test_timeout() -> void:
+	draw_size += 3
+
 
 
 func change_state(new_state : MineState):
@@ -28,7 +41,7 @@ func change_state(new_state : MineState):
 	match current_state:
 		MineState.ARMING:
 			$CollisionShape2D.disabled = true
-			#$sprite.play("")
+			$sprite.play("not_flashing")
 			$arming_timer.start(arming_delay)
 		MineState.ARMED:
 			$sprite.play("flashing")
@@ -42,28 +55,21 @@ func change_state(new_state : MineState):
 			queue_free()
 
 
-func _on_body_entered(body: Node2D) -> void:
-	change_state(MineState.TRIGGERED)
+func _on_body_entered(_body: Node2D) -> void:
+	if current_state != MineState.TRIGGERED:
+		change_state(MineState.TRIGGERED)
 
+func rotate_mine(delta : float):
+	if should_rotate:
+		rotation_degrees += 15 * delta
 
 func explode():
-	#$sprite.play("explode")
+	$sprite.play("explosion")
+	rotate_mine(0)
 	apply_explosion_damage()
 	
-	await get_tree().create_timer(1).timeout# handle explosion countdown (0.3 sec?)
-	change_state(MineState.DEAD)
-	
-	#if who_boom.is_in_group("enemies"):
-		#if who_boom.health == 1:
-			#who_boom.play_death()
-		#else:
-			#who_boom.health -= 1
-		#who_boom.flash()
-	#if who_boom.is_in_group("asteroid"):
-		#who_boom.play_death()
+	#await get_tree().create_timer(8).timeout# handle explosion countdown (0.3 sec?)
 	#change_state(MineState.DEAD)
-
-
 
 
 func apply_explosion_damage():
@@ -81,9 +87,11 @@ func apply_explosion_damage():
 	for res in space_state.intersect_shape(params, 64):
 		var obj = res.collider
 		if obj:
-			if obj.has_method("adjust_health"):
+			if obj.is_in_group("player"):
+				obj.hurt_player(explosion_damage)
+			elif obj.has_method("adjust_health"):
 				obj.adjust_health(explosion_damage)
-			if obj.is_in_group("asteroid"):
+			elif obj.is_in_group("asteroid"):
 				obj.play_death()
 
 
@@ -96,4 +104,9 @@ func _on_arming_timer_timeout() -> void:
 
 func _on_despawn_timer_timeout() -> void:
 	if current_state == MineState.ARMED:
+		change_state(MineState.DEAD)
+
+
+func _on_sprite_animation_finished() -> void:
+	if $sprite.animation == "explosion":
 		change_state(MineState.DEAD)
